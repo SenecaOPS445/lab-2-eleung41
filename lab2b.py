0b8175c0b3c28728f38a05c4a7e22899cf677bd0
Edison Leung <eleung41@myseneca.ca>
2025-01-26 21:45:38 -0500

    added lab2b.py

---
+++ lab2b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Author: Edison Leung
+
+# Prompts user for name and age and stores input as values for objects.
+name = input("Name: ")
+age = input("Age: ")
+
+# Greets the user with the user's inputted name and age
+print('Hi ' + name + ', you are ' + age + ' years old.')
