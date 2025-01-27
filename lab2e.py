f54f3814c591d35914ae4fa75e7bbccaa7198b9c
Edison Leung <eleung41@myseneca.ca>
2025-01-26 21:56:37 -0500

    added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: Edison Leung
+# Author ID: 147724231
+# Date Created: 2025/01/26
+
+# Initialize the timer object with a value of 10
+timer = 10
+
+# Use a while loop to count down from 10 to 1
+while timer > 0:
+    print(timer)
+    timer -= 1  # Decrement the timer by 1
+
+# Print "blast off!" after the countdown ends
+print("blast off!")
+
